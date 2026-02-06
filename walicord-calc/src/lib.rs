05cc3276787db8d98bc1c7a9a1f73d10bdd64f5d
yu256 <122450013+yu256@users.noreply.github.com>
2026-02-06 23:40:55 +0900

    disable coin_cbc logging

--- walicord-calc/src/lib.rs
+++ walicord-calc/src/lib.rs
@@ -65,6 +65,8 @@ pub fn minimize_transactions<'a>(
     }
 
     let mut problem = vars.minimise(objective).using(default_solver);
+    #[cfg(feature = "coin_cbc")]
+    problem.set_parameter("log", "0");
 
     // Constraint: how_much[i][j] <= big_m * who_pays[i][j]
     for ((&hm, &wp), &(_, _, max_amount)) in how_much.iter().zip(&who_pays).zip(&pairs) {
